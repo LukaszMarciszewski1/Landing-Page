e2a305059a91b5eaa2b22e9b7307a773fa657f68
Łukasz Marciszewski <lmarciszewski1@gmail.com>
2021-03-20 13:00:43 +0100

    portfolio init

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// import {Race} from './modules/Race.js';
+import '../scss/style.scss';
+window.addEventListener("DOMContentLoaded", () => {
+    // const race = new Race()
+    console.log('ok')
+});
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/js/index.js',
+    mode: 'development',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js',
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss|sass$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(jpg|png|svg|gif|jpeg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                      name: 'images/[name].[ext]',
+                      outputPath: 'images',
+                    }
+                  }
+            },
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        }),
+        // new CopyPlugin({
+        //     patterns: [
+        //         { from: 'public/images', to: 'images' },
+        //     ],
+        //   }),
+    ]
+};
