145eef7a369d8db99afd09410121d11a2f238575
Łukasz Marciszewski <lmarciszewski1@gmail.com>
2021-03-23 19:58:39 +0100

    contact form

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,29 @@
-// import {Race} from './modules/Race.js';
 import '../scss/style.scss';
 // window.addEventListener("DOMContentLoaded", () => {
 //     console.log('ok')
 // });
 
-class Slider {
-    constructor() {
-        this.selectMainContent = [...document.querySelectorAll('.item')]
-        this.mainContent = [...document.querySelectorAll('.main-content')]
+// class Slider {
+//     constructor() {
+//         this.selectMainContent = [...document.querySelectorAll('.item')]
+//         this.mainContent = [...document.querySelectorAll('.main-content')]
 
-        this.selectMainContent.forEach(select => select.addEventListener('click', () => this.activeContent(select)))
-    }
+//         this.selectMainContent.forEach(select => select.addEventListener('click', () => this.activeContent(select)))
+//     }
 
-    contentPosition() {
-        const activeItem = this.selectMainContent.findIndex(item => item.classList.contains('nav__item--active'))
-        const size = this.mainContent[0].clientHeight
-        this.mainContent.forEach(content => {
-            content.style.transform = 'translateY(' + (-activeItem * size) + 'px)'
-            console.log(activeItem)
-        })
-    }
+//     contentPosition() {
+//         const activeItem = this.selectMainContent.findIndex(item => item.classList.contains('nav__item--active'))
+//         const size = this.mainContent[0].clientHeight
+//         this.mainContent.forEach(content => {
+//             content.style.transform = 'translateY(' + (-activeItem * size) + 'px)'
+//             console.log(activeItem)
+//         })
+//     }
 
-    activeContent(select) {
-        this.selectMainContent.forEach(select => select.classList.remove('nav__item--active'))
-        select.classList.add('nav__item--active')
-        this.contentPosition()
-    }
-}
-const slider = new Slider()
+//     activeContent(select) {
+//         this.selectMainContent.forEach(select => select.classList.remove('nav__item--active'))
+//         select.classList.add('nav__item--active')
+//         this.contentPosition()
+//     }
+// }
+// const slider = new Slider()
