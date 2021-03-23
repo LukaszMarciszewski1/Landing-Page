e17a648541bbc4b32ea32e075d8b22f1fe8fc399
Łukasz Marciszewski <lmarciszewski1@gmail.com>
2021-03-23 11:48:44 +0100

    navigation logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 // import {Race} from './modules/Race.js';
 import '../scss/style.scss';
-window.addEventListener("DOMContentLoaded", () => {
-    // const race = new Race()
-    console.log('ok')
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//     console.log('ok')
+// });
