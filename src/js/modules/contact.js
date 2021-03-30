d4d4783a5a203079cf198d8a9418f7e72dbdd6c5
Łukasz Marciszewski <lmarciszewski1@gmail.com>
2021-03-30 20:40:45 +0200

    validation contact

--- src/js/modules/contact.js
+++ src/js/modules/contact.js
@@ -1,10 +1,19 @@
 export const handleSubmit = (e) => {
     e.preventDefault()
-    let myForm = document.getElementById('contact-form');
-    const name = document.getElementById('name').value;
-    const message = document.getElementById('message').value
-    let formData = new FormData(myForm)
-    fetch("https://formsubmit.co/ajax/lmarciszewski1@gmail.com", {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    const result = document.getElementById('result');
+    const arr = [...document.querySelectorAll('.contact-txt')];
+
+    const clearInputs = () => {
+        setTimeout(() => {
+            result.innerHTML = '';
+            arr.forEach(el => el.value = '')
+        }, 5000)
+    }
+
+    fetch("https://formsubmit.co/ajax/f6fcc616522bf9c63486bd9118f68afa", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -12,11 +21,24 @@ export const handleSubmit = (e) => {
             },
             body: JSON.stringify({
                 name: name,
+                email: email,
                 message: message
             })
-            // body: new URLSearchParams(formData).toString()
         })
-        // .then(response => response.json())
-        .then(() => console.log('Form successfully submitted')).catch((error) =>
-            alert(error))
+        .then(response => response.json())
+        .then(success => {
+            if (success) {
+                result.innerHTML = (
+                    '<p class="success"><ion-icon name="checkmark-circle-outline"></ion-icon>  Wiadomość została wysłana :)</p>')
+                clearInputs()
+            }
+        }).catch((error) => {
+            if (error) {
+                result.innerHTML = (
+                    '<p class="error"><ion-icon name="checkmark-circle-outline"></ion-icon> Wystąpił błąd, spróbuj ponownie (:</p>')
+                console.log(error)
+                clearInputs()
+            }
+        })
+
 }
