81b59403b2168d5700206eefae73ace08e1b2b8f
Łukasz Marciszewski <lmarciszewski1@gmail.com>
2021-03-30 16:28:42 +0200

    contact email

--- src/js/modules/contact.js
+++ src/js/modules/contact.js
@@ -19,13 +19,15 @@
 
 
 export const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('contact-form');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
+    e.preventDefault()
+    let myForm = document.getElementById('contact-form');
+    let formData = new FormData(myForm)
+    fetch('lmarciszewski1@gmail.com', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+        alert(error))
 }
